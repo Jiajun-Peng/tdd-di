172ebaadaacd1ed135261a0d2fb6514d800b96b8
Jiajun-Peng <pengjiajun566@gmail.com>
2024-03-12 22:33:38 +0800

    编写测试类，并按不同的测试范围分组

---
+++ src/test/java/world/nobug/tdd/di/ContainerTest.java
@@ -0,0 +1,24 @@
+package world.nobug.tdd.di;
+
+import org.junit.jupiter.api.Nested;
+
+public class ContainerTest {
+
+    // 组件构造相关的测试类
+    @Nested
+    public class ComponentConstruction{
+
+    }
+
+    // 依赖选择相关的测试类
+    @Nested
+    public class DependenciesSelection{
+
+    }
+
+    // 生命周期管理相关的测试类
+    @Nested
+    public class LifecycleManagement{
+
+    }
+}
