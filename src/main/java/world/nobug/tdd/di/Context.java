e16383c09a0deb49a45c30b0294ed93db2fc5b2b
Jiajun-Peng <pengjiajun566@gmail.com>
2024-03-14 22:44:08 +0800

    重构-同理替换掉componentImplementations，测试通过。

--- src/main/java/world/nobug/tdd/di/Context.java
+++ src/main/java/world/nobug/tdd/di/Context.java
@@ -2,7 +2,6 @@ package world.nobug.tdd.di;
 
 import jakarta.inject.Provider;
 
-import java.lang.reflect.InvocationTargetException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -18,17 +17,16 @@ public class Context {
 
     public <ComponentType, ComponentImplementation extends ComponentType>
     void bind(Class<ComponentType> type, Class<ComponentImplementation> implementation) {
-        componentImplementations.put(type, implementation);
+        providers.put(type, () -> {
+            try {
+                return implementation.getConstructor().newInstance();
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        });
     }
 
     public <ComponentType> ComponentType get(Class<ComponentType> type) {
-        if (providers.containsKey(type))
-            return (ComponentType) providers.get(type).get();
-        Class<?> implementation = componentImplementations.get(type);
-        try {
-            return (ComponentType) implementation.getConstructor().newInstance();
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
+        return (ComponentType) providers.get(type).get();
     }
 }
