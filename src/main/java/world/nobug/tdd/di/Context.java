9f148448773351a35096f8a0443e08a02a4dda38
Jiajun-Peng <pengjiajun566@gmail.com>
2024-08-07 16:00:00 +0800

    编写TODO: instance，并使代码通过编译

---
+++ src/main/java/world/nobug/tdd/di/Context.java
@@ -0,0 +1,12 @@
+package world.nobug.tdd.di;
+
+public class Context {
+
+    public <ComponentType> void bind(Class<ComponentType> type, ComponentType instance) {
+
+    }
+
+    public <ComponentType> ComponentType get(Class<ComponentType> typeClass) {
+        return null;
+    }
+}
--- src/test/java/world/nobug/tdd/di/ContainerTest.java
+++ src/test/java/world/nobug/tdd/di/ContainerTest.java
@@ -1,14 +1,32 @@
 package world.nobug.tdd.di;
 
+import static org.junit.jupiter.api.Assertions.assertSame;
 import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
 
 public class ContainerTest {
+
+    interface Component{
+    }
 
     // 组件构造相关的测试类
     @Nested
     public class ComponentConstruction{
 
         // TODO: instance
+        @Test
+        public void should_bind_type_to_a_specific_instance() {
+
+            Context context = new Context();
+
+            // 创建一个实现了 Component 接口的匿名内部类实例
+            Component instance = new Component() {
+            };
+            context.bind(Component.class, instance);
+
+            assertSame(instance, context.get(Component.class));
+        }
+
         // TODO: abstract class
         // TODO: interface
 
@@ -17,6 +35,16 @@ public class ContainerTest {
             // TODO: No args constructor
             // TODO: with dependencies
             // TODO: A -> B -> C
+        }
+
+        @Nested
+        public class FieldInjection{
+
+        }
+
+        @Nested
+        public class MethodInjection{
+
         }
 
     }
