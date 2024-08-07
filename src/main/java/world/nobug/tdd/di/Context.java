0d40119915ea8303c86bd5d2fdd6f51052cdb96d
Jiajun-Peng <pengjiajun566@gmail.com>
2024-08-07 17:23:14 +0800

    重构，移除components这个map

--- src/main/java/world/nobug/tdd/di/Context.java
+++ src/main/java/world/nobug/tdd/di/Context.java
@@ -6,12 +6,10 @@ import java.util.Map;
 
 public class Context {
 
-    private Map<Class<?>, Object> components = new HashMap<>();
     private Map<Class<?>, Class<?>> componentImplementations = new HashMap<>();
     private Map<Class<?>, Provider<?>> providers = new HashMap<>();
 
     public <ComponentType> void bind(Class<ComponentType> type, ComponentType instance) {
-        components.put(type, instance);
         providers.put(type, () -> instance);
     }
 
