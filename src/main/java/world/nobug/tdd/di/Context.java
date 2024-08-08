fdf34d636161fe2d4e259d7eeaa68035f5c1a2fc
Jiajun-Peng <pengjiajun566@gmail.com>
2024-08-08 16:51:10 +0800

    快速实现，先重构，newInstance时传入需要的依赖的实例，但目前还是使用的默认构造函数，所以还是会失败

--- src/main/java/world/nobug/tdd/di/Context.java
+++ src/main/java/world/nobug/tdd/di/Context.java
@@ -1,6 +1,8 @@
 package world.nobug.tdd.di;
 
 import jakarta.inject.Provider;
+import java.lang.reflect.Constructor;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -16,7 +18,12 @@ public class Context {
     void bind(Class<Type> type, Class<Implementation> implementation) {
         providers.put(type, () -> {
             try {
-                return implementation.getConstructor().newInstance();
+                Constructor<Implementation> injectConstructor = implementation.getConstructor();
+                // 根据构造函数的参数，获取依赖的实例
+                Object[] dependencies = Arrays.stream(injectConstructor.getParameters())
+                        .map(p -> get(p.getType()))
+                        .toArray(Object[]::new);
+                return injectConstructor.newInstance(dependencies);
             } catch (Exception e) {
                 throw new RuntimeException(e);
             }
