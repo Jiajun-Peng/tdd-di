d8111f032f2f7568bd65d1e601f1db1a36e97256
Jiajun-Peng <pengjiajun566@gmail.com>
2024-08-10 11:37:06 +0800

    重构，将 get 方法的实现委托给 getContext 方法，并可以移除掉 get 方法的 Override，也移除掉 ContextConfig 需要实现的接口

--- src/main/java/world/nobug/tdd/di/ContextConfig.java
+++ src/main/java/world/nobug/tdd/di/ContextConfig.java
@@ -10,7 +10,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
-public class ContextConfig implements Context {
+public class ContextConfig {
 
     private Map<Class<?>, Provider<?>> providers = new HashMap<>();
 
@@ -34,9 +34,8 @@ public class ContextConfig implements Context {
         };
     }
 
-    @Override
     public <Type> Optional<Type> get(Class<Type> type) {
-        return Optional.ofNullable(providers.get(type)).map(provider -> (Type) provider.get());
+        return getContext().get(type);
     }
 
     class ConstructorInjectionProvider<T> implements Provider<T>{
