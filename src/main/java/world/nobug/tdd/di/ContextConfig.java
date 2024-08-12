454d18b23a40d72047277fe29869fcdd30fd2067
Jiajun-Peng <pengjiajun566@gmail.com>
2024-08-12 14:51:34 +0800

    重构，将for循环改为foreach循环

--- src/main/java/world/nobug/tdd/di/ContextConfig.java
+++ src/main/java/world/nobug/tdd/di/ContextConfig.java
@@ -29,10 +29,7 @@ public class ContextConfig {
     }
 
     public Context getContext() {
-        // check dependencies
-        for (Class<?> component : dependencies.keySet()) {
-            checkDependencies(component, new Stack<>());
-        }
+        dependencies.keySet().forEach(component -> checkDependencies(component, new Stack<>()));
         return new Context() {
             @Override
             public <Type> Optional<Type> get(Class<Type> type) {
