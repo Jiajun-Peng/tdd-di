fe78a01bc609e7a810a18b31ece9b3b2aad9ac2f
Jiajun-Peng <pengjiajun566@gmail.com>
2024-08-12 15:13:31 +0800

    重构，移除掉ConstructorInjectionProvider中不再使用的字段

--- src/main/java/world/nobug/tdd/di/ContextConfig.java
+++ src/main/java/world/nobug/tdd/di/ContextConfig.java
@@ -24,7 +24,7 @@ public class ContextConfig {
     void bind(Class<Type> type, Class<Implementation> implementation) {
         Constructor<Implementation> injectConstructor = getInjectConstructor(implementation);
 
-        providers.put(type, new ConstructorInjectionProvider(type, injectConstructor));
+        providers.put(type, new ConstructorInjectionProvider(injectConstructor));
         dependencies.put(type, Arrays.asList(injectConstructor.getParameterTypes()));
     }
 
@@ -56,11 +56,9 @@ public class ContextConfig {
     }
 
     class ConstructorInjectionProvider<T> implements ComponentProvider<T>{
-        private Class<?> componentType;
         private Constructor<T> injectConstructor;
 
-        public ConstructorInjectionProvider(Class<?> componentType, Constructor<T> injectConstructor) {
-            this.componentType = componentType;
+        public ConstructorInjectionProvider(Constructor<T> injectConstructor) {
             this.injectConstructor = injectConstructor;
         }
 
