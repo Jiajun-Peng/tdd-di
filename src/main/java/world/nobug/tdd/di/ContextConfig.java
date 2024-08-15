4905e47e0c9c1cdb4c0415531c2e329f0a946710
Jiajun-Peng <pengjiajun566@gmail.com>
2024-08-15 16:35:13 +0800

    实现 could get Provider<T> from context 的 sad path 功能

--- src/main/java/world/nobug/tdd/di/ContextConfig.java
+++ src/main/java/world/nobug/tdd/di/ContextConfig.java
@@ -31,6 +31,7 @@ public class ContextConfig {
 
             @Override
             public Optional get(ParameterizedType type) {
+                if (type.getRawType() != Provider.class) return Optional.empty();
                 Class<?> componentType = (Class<?>)type.getActualTypeArguments()[0];
                 return Optional.ofNullable(providers.get(componentType))
                         .map(provider -> (Provider<Object>) () -> provider.get(this));
