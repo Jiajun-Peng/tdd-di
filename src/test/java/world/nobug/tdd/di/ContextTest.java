d13bf4aeb7436bc96956a6ea864b571e1297ae95
Jiajun-Peng <pengjiajun566@gmail.com>
2024-08-17 11:56:36 +0800

    重构-移除TypeLiteral

--- src/test/java/world/nobug/tdd/di/ContextTest.java
+++ src/test/java/world/nobug/tdd/di/ContextTest.java
@@ -7,7 +7,6 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import jakarta.inject.Inject;
 import jakarta.inject.Provider;
-import java.lang.reflect.ParameterizedType;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -15,7 +14,6 @@ import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Stream;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Named;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -141,7 +139,6 @@ public class ContextTest {
             config.bind(Component.class, component);
             Context context = config.getContext();
 
-            ParameterizedType type = new TypeLiteral<Provider<Component>>(){}.getType();
             Provider<Component> provider = context.get(new Context.Ref<Provider<Component>>() {}).get();
 
             assertSame(component, provider.get());
@@ -154,27 +151,9 @@ public class ContextTest {
             config.bind(Component.class, component);
             Context context = config.getContext();
 
-            ParameterizedType type = new TypeLiteral<List<Component>>(){}.getType();
             Optional<List<Component>> components = context.get(new Context.Ref<List<Component>>() {});
 
             assertFalse(components.isPresent());
-        }
-
-        @Test
-        @Disabled
-        public void java_api() {
-            Component component = new Component() {
-            };
-            ParameterizedType type = new TypeLiteral<Provider<Component>>(){}.getType();
-
-            assertEquals(Provider.class, type.getRawType());
-            assertEquals(Component.class, type.getActualTypeArguments()[0]);
-        }
-
-        static abstract class TypeLiteral<T> {
-            public ParameterizedType getType() {
-                return (ParameterizedType) ((ParameterizedType)(getClass().getGenericSuperclass())).getActualTypeArguments()[0];
-            }
         }
 
     }
