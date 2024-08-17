1af9a1b23076d4cd19c2104d1e8d6c382a025806
Jiajun-Peng <pengjiajun566@gmail.com>
2024-08-17 09:55:33 +0800

    重构-修改Context对外接口

--- src/main/java/world/nobug/tdd/di/Context.java
+++ src/main/java/world/nobug/tdd/di/Context.java
@@ -4,7 +4,9 @@ import java.lang.reflect.Type;
 import java.util.Optional;
 
 public interface Context {
-    Optional get(Type type);
+    default Optional get(Type type) {
+        return get(Ref.of(type));
+    }
 
     Optional get(Ref ref);
 }
--- src/main/java/world/nobug/tdd/di/ContextConfig.java
+++ src/main/java/world/nobug/tdd/di/ContextConfig.java
@@ -24,10 +24,6 @@ public class ContextConfig {
     public Context getContext() {
         providers.keySet().forEach(component -> checkDependencies(component, new Stack<>()));
         return new Context() {
-            @Override
-            public Optional get(Type type) {
-                return get(Ref.of(type));
-            }
 
             @Override
             public Optional<?> get(Ref ref) {
