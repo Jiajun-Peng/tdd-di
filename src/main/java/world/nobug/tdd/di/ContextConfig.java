6a50bce22108f10d674c68b3f2547d2bbc564e79
Jiajun-Peng <pengjiajun566@gmail.com>
2024-08-19 14:59:12 +0800

    重构，使用 components 替换掉 providers

--- src/main/java/world/nobug/tdd/di/ContextConfig.java
+++ src/main/java/world/nobug/tdd/di/ContextConfig.java
@@ -2,8 +2,6 @@ package world.nobug.tdd.di;
 
 import jakarta.inject.Provider;
 import java.lang.annotation.Annotation;
-import java.lang.reflect.Type;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -12,12 +10,10 @@ import java.util.Stack;
 
 public class ContextConfig {
 
-    private Map<Class<?>, ComponentProvider<?>> providers = new HashMap<>();
-
     private Map<Component, ComponentProvider<?>> components = new HashMap<>();
 
     public <Type> void bind(Class<Type> type, Type instance) {
-        providers.put(type, (ComponentProvider<Type>) context -> instance);
+        components.put(new Component(type, null), context -> instance);
     }
 
     public <Type> void bind(Class<Type> type, Type instance, Annotation... qualifiers) {
@@ -30,7 +26,7 @@ public class ContextConfig {
 
     public <Type, Implementation extends Type>
     void bind(Class<Type> type, Class<Implementation> implementation) {
-        providers.put(type, new InjectionProvider(implementation));
+        components.put(new Component(type, null), new InjectionProvider(implementation));
     }
 
     public <Type, Implementation extends Type>
@@ -40,36 +36,37 @@ public class ContextConfig {
     }
 
     public Context getContext() {
-        providers.keySet().forEach(component -> checkDependencies(component, new Stack<>()));
+        components.keySet().forEach(component -> checkDependencies(component, new Stack<>()));
         return new Context() {
 
             @Override
             public <ComponentType> Optional<ComponentType> get(Ref<ComponentType> ref) {
-                if (ref.getQualifier() != null) return (Optional<ComponentType>) Optional.ofNullable(components.get(
-                        new Component(ref.getComponent(), ref.getQualifier())
-                )).map(provider -> provider.get(this));
-
                 if (ref.isContainer()) {
                     if (ref.getContainer() != Provider.class) return Optional.empty();
-                    return (Optional<ComponentType>) Optional.ofNullable(providers.get(ref.getComponent()))
+                    return (Optional<ComponentType>) Optional.ofNullable(getComponentProvider(ref))
                             .map(provider -> (Provider<Object>) () -> provider.get(this));
                 }
-                return (Optional<ComponentType>) Optional.ofNullable(providers.get(ref.getComponent())).map(provider -> provider.get(this));
+                return (Optional<ComponentType>) Optional.ofNullable(getComponentProvider(ref)).map(provider -> provider.get(this));
             }
 
         };
     }
 
+    private <ComponentType> ComponentProvider<?> getComponentProvider(Context.Ref<ComponentType> ref) {
+        return components.get(new Component(ref.getComponent(), ref.getQualifier()));
+    }
+
     // 深度优先遍历 检查 component 的依赖的访问记录
     // visiting 保存正在被访问的记录，如果发现正在被访问的记录再次被访问，说明存在循环依赖
-    private void checkDependencies(Class<?> component, Stack<Class<?>> visiting) {
-        for (Context.Ref dependency : providers.get(component).getDependencies()) {
+    private void checkDependencies(Component component, Stack<Class<?>> visiting) {
+        for (Context.Ref dependency : components.get(component).getDependencies()) {
             // 如果依赖的类型不存在，就提前停止递归
-            if (!providers.containsKey(dependency.getComponent())) throw new DependencyNotFoundException(component, dependency.getComponent());
+            if (!components.containsKey(new Component(dependency.getComponent(), dependency.getQualifier())))
+                throw new DependencyNotFoundException(component.type, dependency.getComponent());
             if (!dependency.isContainer()) {
                 if (visiting.contains(dependency.getComponent())) throw new CyclicDependenciesException(visiting);
                 visiting.push(dependency.getComponent());
-                checkDependencies(dependency.getComponent(), visiting);
+                checkDependencies(new Component(dependency.getComponent(), dependency.getQualifier()), visiting);
                 visiting.pop();
             }
         }
